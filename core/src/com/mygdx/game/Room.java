245f381e83b773fc26632a19d35d8c887c107fb0
Anenkov_Oleg <anenkovoleg04@gmail.com>
2024-04-30 18:29:52 +0300

    Create Room

---
+++ core/src/com/mygdx/game/Room.java
@@ -0,0 +1,23 @@
+package com.mygdx.game;
+
+public class Room implements Scene{
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
