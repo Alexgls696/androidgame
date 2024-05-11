281bb5388c48005eea4d83395bad7957d1b8483d
SolovyovDaniil <daniilsolovyov5@gmail.com>
2024-05-11 16:51:46 +0300

    Добавлено масштабирование в меню игр

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -44,84 +44,13 @@ public class Game implements Scene{
     float sleepStateTime = 0;
     private SpriteBatch sleep_sprite;
     private Texture sleep_texture;
+    private float coefficientX;
+    private float coefficientY;
     BitmapFont font = new BitmapFont(Gdx.files.internal("font.fnt"));
-    /*private void init_buttons() {
-        ImageButton imageButtonGames = new ImageButton(new TextureRegionDrawable(new Texture("button_games.png")), new TextureRegionDrawable(new Texture("button_games.png")));
-        imageButtonGames.setPosition(210, 100);
-        imageButtonGames.getImage().setFillParent(true);
-
-        ImageButton imageButtonRoom = new ImageButton(new TextureRegionDrawable(new Texture("button_room.png")), new TextureRegionDrawable(new Texture("button_room.png")));
-        imageButtonRoom.setPosition(380, 100);
-        imageButtonRoom.getImage().setFillParent(true);
-
-        ImageButton imageButtonKitchen = new ImageButton(new TextureRegionDrawable(new Texture("button_kitchen.png")), new TextureRegionDrawable(new Texture("button_kitchen.png")));
-        imageButtonKitchen.setPosition(550, 100);
-        imageButtonKitchen.getImage().setFillParent(true);
-
-        ImageButton imageButtonBedroom = new ImageButton(new TextureRegionDrawable(new Texture("button_bedroom.png")), new TextureRegionDrawable(new Texture("button_bedroom.png")));
-        imageButtonBedroom.setPosition(720, 100);
-        imageButtonBedroom.getImage().setFillParent(true);
-        imageButtonGames.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                MyGdxGame.scene= scene_games;
-                scene_games.action();
-                Gdx.input.setInputProcessor(MyGdxGame.scene_games.getStage());
-                return super.touchDown(event, x, y, pointer, button);
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                super.touchUp(event, x, y, pointer, button);
-            }
-        });
-
-        imageButtonRoom.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                MyGdxGame.scene=MyGdxGame.scene_room;
-                Gdx.input.setInputProcessor(MyGdxGame.scene_room.getStage());
-                return super.touchDown(event, x, y, pointer, button);
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                super.touchUp(event, x, y, pointer, button);
-            }
-        });
-
-        imageButtonKitchen.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return super.touchDown(event, x, y, pointer, button);
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                super.touchUp(event, x, y, pointer, button);
-            }
-        });
-
-        imageButtonBedroom.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                MyGdxGame.scene=MyGdxGame.scene_bedroom;
-                Gdx.input.setInputProcessor(MyGdxGame.scene_bedroom.getStage());
-                return super.touchDown(event, x, y, pointer, button);
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                super.touchUp(event, x, y, pointer, button);
-            }
-        });
-        stage.addActor(imageButtonGames);
-        stage.addActor(imageButtonRoom);
-        stage.addActor(imageButtonKitchen);
-        stage.addActor(imageButtonBedroom);
-    }*/
     @Override
     public void create() {
+        coefficientX = Gdx.graphics.getWidth() / 1080f;
+        coefficientY = Gdx.graphics.getHeight() / 2400f;
         stage = new Stage(new ScreenViewport());
         rectangle = new Rectangle();
         games = new ArrayList<>();
@@ -138,7 +67,8 @@ public class Game implements Scene{
         sleep_texture = new Texture("Game/message_sleep.png");
         //init_buttons();
         ImageButton flappyBirdButton = new ImageButton(new TextureRegionDrawable(new Texture("Game/FlappyBird_icon.png")), new TextureRegionDrawable(new Texture("Game/FlappyBird_icon.png")));
-        flappyBirdButton.setPosition(Gdx.graphics.getWidth() * 0.18f, Gdx.graphics.getHeight() - flappyBirdButton.getHeight() - 400);
+        flappyBirdButton.setPosition(190 * coefficientX, 1500 * coefficientY);
+        flappyBirdButton.setSize(700 * coefficientX, 484 * coefficientY);
         flappyBirdButton.addListener(new ClickListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -153,7 +83,8 @@ public class Game implements Scene{
             }
         });
         ImageButton dinoGameButton = new ImageButton(new TextureRegionDrawable(new Texture("Game/DinoGame_icon.png")), new TextureRegionDrawable(new Texture("Game/DinoGame_icon.png")));
-        dinoGameButton.setPosition(Gdx.graphics.getWidth() * 0.18f, Gdx.graphics.getHeight() - flappyBirdButton.getHeight() * 2 - 450);
+        dinoGameButton.setPosition(190 * coefficientX, 950 * coefficientY);
+        dinoGameButton.setSize(700 * coefficientX, 484 * coefficientY);
         dinoGameButton.addListener(new ClickListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -168,7 +99,8 @@ public class Game implements Scene{
             }
         });
         ImageButton gymButton = new ImageButton(new TextureRegionDrawable(new Texture("Game/gym_icon.png")), new TextureRegionDrawable(new Texture("Game/gym_icon.png")));
-        gymButton.setPosition(Gdx.graphics.getWidth() * 0.18f, Gdx.graphics.getHeight() - flappyBirdButton.getHeight() * 3 - 500);
+        gymButton.setPosition(190 * coefficientX, 400 * coefficientY);
+        gymButton.setSize(700 * coefficientX, 484 * coefficientY);
         gymButton.addListener(new ClickListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -185,7 +117,7 @@ public class Game implements Scene{
         stage.addActor(gymButton);
         stage.addActor(flappyBirdButton);
         stage.addActor(dinoGameButton);
-        //action();
+        action();
     }
 
     @Override
